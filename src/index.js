d588a5d24a040f7ddd3c96927a1ae0f5f22e490d
dhruv <idhruvjit@gmail.com>
2019-11-15 20:11:58 +0530

    minor change pushed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,4 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css'
 /*get app elementbyID is to get element from index.html where we want to output*/
-ReactDOM.render(
-        <App />,
-    document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('app'));
